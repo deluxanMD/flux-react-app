68311b66e5fb972f5456717721a4b0052c54a361
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-04-29 20:51:05 +0530

    forgot to remove

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,6 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 import { ReduxWrapper } from "test-utils/redux-test-utils";
-import themereducer, {
-  initialState,
-  setMode,
-} from "./global/theme/theme.slice";
 
 describe("<App />", () => {
   test("renders learn react link", () => {
@@ -15,15 +11,15 @@ describe("<App />", () => {
     );
   });
 
-  test("toggle theme", () => {
-    render(
-      <ReduxWrapper>
-        <App />
-      </ReduxWrapper>
-    );
+  // test("toggle theme", () => {
+  //   render(
+  //     <ReduxWrapper>
+  //       <App />
+  //     </ReduxWrapper>
+  //   );
 
-    expect(themereducer(initialState, setMode("dark"))).toEqual({
-      mode: "dark",
-    });
-  });
+  //   expect(themereducer(initialState, setMode("dark"))).toEqual({
+  //     mode: "dark",
+  //   });
+  // });
 });
